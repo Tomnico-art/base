b8e7850e8457debb446f0df5c66c0d7d84b5b5de
peng <peng@example.com>
2023-10-16 21:32:16 +0800

    add c test

--- c/c++/c/test33.cpp
+++ c/c++/c/test33.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class A
@@ -9,6 +10,21 @@ class A
 int main()
 {
     cout << sizeof(A) << endl;
+
+    vector<int> list1 = {1,2,3,4,5,6};
+    vector<int> list1_1 = {1,2,3,4,5,6};
+
+    vector<int> list2;//元素为空
+
+    vector<int> list3 = list1;
+
+    vector<int> list4(list1.begin()+2, list1.end()-1);  //{3,4,5}
+
+    vector<int> list5(10); //10个0；
+
+    vector<int> list6(10, 3);    //10个3
+
+
 
     return 0;
 }
